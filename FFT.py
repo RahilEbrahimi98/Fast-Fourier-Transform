b4787e66019acd38b34afc374e93f1df54b97458
FatemehEbrahimi98 <rahil.farzanegan@gmail.com>
2021-08-27 15:32:31 +0430

    Update FFT.py

--- FFT.py
+++ FFT.py
@@ -80,41 +80,41 @@ def plot(pointsArray):
     
     ##python result:
     
-#     result = np.fft.fft(pointsArray)
-#     absResult = [abs(x) for x in result]
-#     print("python ans is: ")
-#     print(result)
-#     phaseResult = [cmath.phase(x) for x in result]
-#     plt.subplot(5, 1, 1) 
-#     plt.title('X[n]') 
-#     plt.plot([x for x in range(len(pointsArray))], pointsArray, 'og') 
-#     plt.subplot(5, 1, 2) 
-#     plt.title('FFT abs') 
-#     plt.xlabel('w abs') 
-#     plt.ylabel('|H(e^jw)|') 
-#     plt.plot([x for x in range(len(pointsArray))], absResult, 'r') 
-#     plt.subplot(5, 1, 3) 
-#     plt.title('FFT phase') 
-#     plt.xlabel('w phase') 
-#     plt.ylabel('phase H(e^jw)') 
-#     plt.plot([x for x in range(len(pointsArray))], phaseResult, 'r')
-#     plt.subplot(5, 1, 4)
-#     plt.title('FFT real') 
-#     plt.xlabel('w ') 
-#     plt.ylabel('real')
-#     resultreal=[np.real(x) for x in result]
-#     plt.plot([x for x in range(len(pointsArray))], resultreal, 'g')
-#     plt.subplot(5, 1, 5)
-#     plt.title('FFT image') 
-#     plt.xlabel('w ') 
-#     plt.ylabel('image')
-#     resultimage=[np.imag(x) for x in result]
-#     plt.plot([x for x in range(len(pointsArray))], resultimage, 'g')
-#     plt.show()
+    result = np.fft.fft(pointsArray)
+    absResult = [abs(x) for x in result]
+    print("python ans is: ")
+    print(result)
+    phaseResult = [cmath.phase(x) for x in result]
+    plt.subplot(5, 1, 1) 
+    plt.title('X[n]') 
+    plt.plot([x for x in range(len(pointsArray))], pointsArray, 'og') 
+    plt.subplot(5, 1, 2) 
+    plt.title('FFT abs') 
+    plt.xlabel('w abs') 
+    plt.ylabel('|H(e^jw)|') 
+    plt.plot([x for x in range(len(pointsArray))], absResult, 'r') 
+    plt.subplot(5, 1, 3) 
+    plt.title('FFT phase') 
+    plt.xlabel('w phase') 
+    plt.ylabel('phase H(e^jw)') 
+    plt.plot([x for x in range(len(pointsArray))], phaseResult, 'r')
+    plt.subplot(5, 1, 4)
+    plt.title('FFT real') 
+    plt.xlabel('w ') 
+    plt.ylabel('real')
+    resultreal=[np.real(x) for x in result]
+    plt.plot([x for x in range(len(pointsArray))], resultreal, 'g')
+    plt.subplot(5, 1, 5)
+    plt.title('FFT image') 
+    plt.xlabel('w ') 
+    plt.ylabel('image')
+    resultimage=[np.imag(x) for x in result]
+    plt.plot([x for x in range(len(pointsArray))], resultimage, 'g')
+    plt.show()
 
 
 #print(np.fft.fft([1,2,3,4,4,3,2,1]))
-# plot([1,2,3,4,4,3,2])
+#plot([1,2,3,4,4,3,2])
 #print(np.fft.fft([math.sin(x) for x in range(8)]))
 
 # 
